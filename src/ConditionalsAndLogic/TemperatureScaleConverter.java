e5b8378bae52521aa6f9e796e3dd38ca0c891367
Keerthika <Keerthika1012000@gmail.com>
2026-08-08 15:14:13 +0530

    Complete Conditionals and Logic problem

---
+++ src/ConditionalsAndLogic/TemperatureScaleConverter.java
@@ -0,0 +1,27 @@
+package ConditionalsAndLogic;
+
+import java.util.Scanner;
+
+public class TemperatureScaleConverter {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        char scale = sc.next().charAt(0);
+
+
+        if(scale == 'C'){
+            double F = (T * 9.0/5)+32;
+            double K = T+273.15;
+            System.out.println("F=" +  F +", K=" + K);
+        }else if(scale == 'F'){
+            double C = (T-32)/1.8;
+            double K = C+273.15;
+            System.out.println("C=" +  C +", K=" + K);
+        }else if(scale == 'K'){
+            double C = T-273.15;
+            double F= (C * 9.0/5)+32;
+            System.out.println("C=" +  C +", F=" + F);
+        }
+    }
+}
