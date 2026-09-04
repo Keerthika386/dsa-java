42e4a8c25a80ca9368a4a76479d6555c5f1a1403
Keerthika <Keerthika1012000@gmail.com>
2026-09-04 17:14:45 +0530

    Solve AdvancedPatternPrinting

--- src/Sprint2/AdvancedPatternPrinting/SpiralMatrixClockwiseFill.java
+++ src/Sprint2/AdvancedPatternPrinting/SpiralMatrixClockwiseFill.java
@@ -7,8 +7,52 @@ public class SpiralMatrixClockwiseFill {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
 
         int left = 0;
-        
+        int right = n-1;
+        int top = 0;
+        int bottom = n-1;
+
+        int count = 1;
+
+        while(top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[left][i] = count;
+                count++;
+            }
+
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][bottom] = count;
+                count++;
+            }
+
+            right--;
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = count;
+                count++;
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = count;
+                count++;
+            }
+            left++;
+        }
+
+
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+
     }
 }
